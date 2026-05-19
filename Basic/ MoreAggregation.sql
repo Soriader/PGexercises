115eae989724fc93379d46504ec514e8fdf3ee28
NewLavoisier <kacperjwawrzyniak@gmail.com>
2026-05-19 07:53:43 +0200

    [~] Basic: More aggregation

---
+++ Basic/ MoreAggregation.sql
@@ -0,0 +1,7 @@
+-- You'd like to get the first and last name of the last member(s)
+--who signed up - not just the date. How can you do that?
+
+SELECT firstname, surname,joindate
+FROM cd.members
+ORDER BY joindate desc
+LIMIT 1;
