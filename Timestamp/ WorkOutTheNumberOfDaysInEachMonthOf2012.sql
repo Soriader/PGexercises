4855f0e4055111d982d1b4b8784f31afa75e0a38
NewLavoisier <kacperjwawrzyniak@gmail.com>
2026-06-03 17:23:47 +0200

    [~] Timestamp: Work out the number of days in each month of 2012

---
+++ Timestamp/ WorkOutTheNumberOfDaysInEachMonthOf2012.sql
@@ -0,0 +1,12 @@
+-- For each month of the year in 2012, output the number of days in that month.
+-- Format the output as an integer column containing the month of the year,
+-- and a second column containing an interval data type.
+
+SELECT
+    EXTRACT(MONTH FROM month_start)::int AS month,
+    (month_start + INTERVAL '1 month') - month_start AS length
+FROM generate_series(
+    TIMESTAMP '2012-01-01',
+    TIMESTAMP '2012-12-01',
+    INTERVAL '1 month'
+) AS months(month_start);
