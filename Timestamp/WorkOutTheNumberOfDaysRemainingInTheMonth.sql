bcb56081c38cb0f11a38022a936f2d17a3579b92
NewLavoisier <kacperjwawrzyniak@gmail.com>
2026-06-03 17:36:43 +0200

    [~] Timestamp: Work out the number of days remaining in the month

---
+++ Timestamp/WorkOutTheNumberOfDaysRemainingInTheMonth.sql
@@ -0,0 +1,9 @@
+-- For any given timestamp, work out the number of days remaining in the month.
+-- The current day should count as a whole day, regardless of the time. Use '2012-02-11 01:00:00'
+-- as an example timestamp for the purposes of making the answer.
+-- Format the output as a single interval value.
+
+
+SELECT
+    (date_trunc('month', TIMESTAMP '2012-02-11 01:00:00') + INTERVAL '1 month')
+    - date_trunc('day', TIMESTAMP '2012-02-11 01:00:00') AS remaining;
